48cd8c44283027a89bcf370e6addb8f5e2e68d35
nikhil andola <nikhilandola123@gmail.com>
2021-10-26 19:48:39 +0530

    Adding components to the Redux shop  App

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div>
+            <h2>Buy Addidas shoes 50 rs</h2>
+            <button className="btn btn-primary mx-2">-</button>
+            Add to cart
+            <button className="btn btn-primary mx-2">+</button>
+        </div>
+    )
+}
+
+export default Shop
